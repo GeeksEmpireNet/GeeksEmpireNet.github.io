6356fa9c767b2efd36b172682951e6469446d60f
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-06-07 10:36:40 -0800

    Initial

--- functions/index.js
+++ functions/index.js
@@ -12,13 +12,65 @@ const runtimeOptions = {
     timeoutSeconds: 313,
 }
 
-const runtimeOptionsSendingEmails = {
-    timeoutSeconds: 500,
-}
+/*[Manager Json Database]*/
+exports.addJsonDatabaseToFirestore = functions.runWith(runtimeOptions).https.onRequest((req, res) => {
+
+    var jsonDatabase = fs.readFileSync('database.json');
+    var jsonContent = JSON.parse(jsonDatabase);
+
+    const firestore = admin.firestore();
+
+    firestore.collection('Android').doc('FloatIt').set({
+        PromotionCodes: jsonContent
+    })
+        .then(ref => {
+
+            res.status(200);
+
+        })
+        .catch(err => {
+
+            console.log(err);
+
+        });
+
+    return "Json Database Imported";
+});
+
+/*[Retrieve Promotion Code]*/
+exports.takePromotionCodeFloatIt = functions.runWith(runtimeOptions).https.onRequest((req, res) => {
+
+    const ipAddressFastlyIP = req.headers['fastly-client-ip'];
+    console.log(`${ipAddressFastlyIP}`);
+
+    const firestore = admin.firestore();
+
+    firestore.collection('Android').doc('FloatIt').get()
+        .then(doc => {
+            if (!doc.exists) {
+
+            } else {
+
+                let documentData = doc.data();
+
+                let aPromotionCode = documentData.PromotionCodes[0];
+                console.log('Used Promotion Code ', aPromotionCode);
+
+                var floatItPromotionCodeLink = 'http://GeeksEmpire.net/Projects/Android/FloatIt/FloatItPromotionCode.html?PromotionCode=' + aPromotionCode;
+                res.redirect(floatItPromotionCodeLink)
+
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    return "Taking A Promotion Code";
+});
 
 /*[List All Users]*/
 exports.retrieveAllUsersData = functions.runWith(runtimeOptions).https.onRequest((req, res) => {
-    listAllUsers();    
+    listAllUsers();
 
     return "Sending Email To Users";
 });
@@ -26,35 +78,36 @@ exports.retrieveAllUsersData = functions.runWith(runtimeOptions).https.onRequest
 function listAllUsers(nextPageToken) {
     // List batch of users, 1000 at a time.
     admin.auth().listUsers(1000, nextPageToken)
-      .then(function(listUsersResult) {
-        listUsersResult.users.forEach(function(userRecord) {
-            var userData = userRecord.toJSON();
-            console.log('User Data :::' + userRecord.displayName + " " + userData.email);
+        .then(function (listUsersResult) {
+            listUsersResult.users.forEach(function (userRecord) {
+                var userData = userRecord.toJSON();
+                console.log('User Data :::' + userRecord.displayName + " " + userData.email);
+            });
+            if (listUsersResult.pageToken) {
+                // List next batch of users.
+                listAllUsers(listUsersResult.pageToken);
+            }
+        })
+        .catch(function (error) {
+            console.log('Error listing users:', error);
         });
-        if (listUsersResult.pageToken) {
-          // List next batch of users.
-          listAllUsers(listUsersResult.pageToken);
-        }
-    })
-    .catch(function(error) {
-        console.log('Error listing users:', error);
-    });
-}  
+}
 
 /*[Send New Promotion Email To Users]*/
-exports.sendPromotionalEmail = functions.runWith(runtimeOptionsSendingEmails).https.onRequest((req, res) => {
+exports.sendPromotionalEmail = functions.runWith(runtimeOptions).https.onRequest((req, res) => {
 
-    /*Run Send Email Through Loop Of Target Users*/ 
-    var emailStringArray = ['m.keleshev@gmail.com',
-    'jamie.bacolod@gmail.com',
-    'dasuttam654321@gmail.com',
-    'krzysiekrozmyty@gmail.com',
-    'kafuhr77@gmail.com',
-    'tiful201810@gmail.com',
-    'ola.d.sether@gmail.com',
-    'sel_kah_28@hotmail.com',
+    /*Run Send Email Through Loop Of Target Users*/
+    var emailStringArray = [
+        'm.keleshev@gmail.com',
+        'jamie.bacolod@gmail.com',
+        'dasuttam654321@gmail.com',
+        'krzysiekrozmyty@gmail.com',
+        'kafuhr77@gmail.com',
+        'tiful201810@gmail.com',
+        'ola.d.sether@gmail.com',
+        'sel_kah_28@hotmail.com',
     ];
-    for (var i = 0; i < emailStringArray.length; i++) {        
+    for (var i = 0; i < emailStringArray.length; i++) {
         sendEmailToUsersFloatIt(emailStringArray[i])
     }
 
@@ -69,11 +122,11 @@ const gmailPassword = "dgzkjtnwmhsxydeb";//AppPassword
 //omdcpzvbpxdcmwyq
 //dgzkjtnwmhsxydeb
 const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
+    service: 'gmail',
+    auth: {
+        user: gmailEmail,
+        pass: gmailPassword,
+    },
 });
 
 var emailSentCounter = 0;
@@ -88,10 +141,10 @@ async function sendEmailToUsersFloatIt(email) {
     //The user subscribed to the newsletter.
     mailOptions.subject = '#FloatIt 🎈 To Become Master Of Multitasking | #Android';
     //mailOptions.text = `Hey ${email || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-    var templateHtml = fs.readFileSync('FloatIt.html',"utf-8").toString();
+    var templateHtml = fs.readFileSync('FloatIt.html', "utf-8").toString();
     mailOptions.html = templateHtml
-    await mailTransport.sendMail(mailOptions, function(error, info){
-        if(error){
+    await mailTransport.sendMail(mailOptions, function (error, info) {
+        if (error) {
             return console.log('Error: ' + error + ' | ' + email);
         }
         emailSentCounter = emailSentCounter + 1;
@@ -111,10 +164,10 @@ async function sendEmailToUsersFloatingWidgets(email) {
     //The user subscribed to the newsletter.
     mailOptions.subject = 'Check Out Floating Widgets | #FloatIt 🎈 ';
     //mailOptions.text = `Hey ${email || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-    var templateHtml = fs.readFileSync('FloatIt.html',"utf-8").toString();
+    var templateHtml = fs.readFileSync('FloatItWidgets.html', "utf-8").toString();
     mailOptions.html = templateHtml
-    await mailTransport.sendMail(mailOptions, function(error, info){
-        if(error){
+    await mailTransport.sendMail(mailOptions, function (error, info) {
+        if (error) {
             return console.log('Error: ' + error + ' | ' + email);
         }
         emailSentCounter = emailSentCounter + 1;
